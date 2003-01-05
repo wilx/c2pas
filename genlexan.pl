3866d857fff69efb4df199decc70b91a28d5ceb5
wilx <unknown>
2003-01-05 15:03:09 +0000

    Pouzito perltidy.

--- genlexan.pl
+++ genlexan.pl
@@ -4,63 +4,65 @@ use Data::Dumper;
 use strict;
 
 sub genname {
-    if ($_[0] =~ /^(?i:[\da-z])+$/) {
-	return $_[0];
+    if ( $_[0] =~ /^(?i:[\da-z])+$/ ) {
+        return $_[0];
     }
     else {
-	return $_[1];
+        return $_[1];
     }
 }
 
-my (%words, $state, $finish, @table);
+my ( %words, $state, $finish, @table );
 
-while (my $line = <STDIN>) {
+while ( my $line = <STDIN> ) {
     next if $line =~ /(?:^\s*(?:\#)|^\s*$)/;
     chomp $line;
     $line =~ /^\s*([^\s]*)\s+([^\s]*)\s*$/;
-    $words{$1}{'token'} = $2;
-    $words{$1}{'str'} = $1;
+    $words{$1}{'token'}     = $2;
+    $words{$1}{'str'}       = $1;
     $words{$1}{'laststate'} = 0;
     print "\"$1\" \"$2\"\n";
 }
 
-$state = 0;
+$state  = 0;
 $finish = 0;
-while (! $finish) {
+while ( !$finish ) {
     $finish = 1;
-    for my $word (sort keys %words) {
-	my $rest = \$words{$word}{'str'};
-	my $laststate = \$words{$word}{'laststate'};
-	if ($$rest) {
-	    # urizni prvni znak
-	    $$rest =~ s/^(.)(.*)$/$2/;
-	    my $char = $1;
-	    if (! defined($table[$$laststate]{'row'}[ord $char])) {
-		$table[$$laststate]{'row'}[ord $char] = ++$state;
-		if (! defined($table[$$laststate]{'prefix'})) {
-		    $table[$$laststate]{'prefix'} = "";
-		}
-		$table[$state]{'prefix'} = $table[$$laststate]{'prefix'}.$char;
-		$$laststate = $state;
-	    }
-	    else {
-		$$laststate = $table[$$laststate]{'row'}[ord $char];
-	    }
-	    
-	    if (! $$rest) {
-		$table[$$laststate]{'token'} = $words{$word}{'token'};
-	    }
-	    $finish = 0;
-	}
+    for my $word ( sort keys %words ) {
+        my $rest      = \$words{$word}{'str'};
+        my $laststate = \$words{$word}{'laststate'};
+        if ($$rest) {
+
+            # urizni prvni znak
+            $$rest =~ s/^(.)(.*)$/$2/;
+            my $char = $1;
+            if ( !defined( $table[$$laststate]{'row'}[ ord $char ] ) ) {
+                $table[$$laststate]{'row'}[ ord $char ] = ++$state;
+                if ( !defined( $table[$$laststate]{'prefix'} ) ) {
+                    $table[$$laststate]{'prefix'} = "";
+                }
+                $table[$state]{'prefix'} =
+                  $table[$$laststate]{'prefix'} . $char;
+                $$laststate = $state;
+            }
+            else {
+                $$laststate = $table[$$laststate]{'row'}[ ord $char ];
+            }
+
+            if ( !$$rest ) {
+                $table[$$laststate]{'token'} = $words{$word}{'token'};
+            }
+            $finish = 0;
+        }
     }
 }
 
 # generuj lexan.hxx
 {
-    my ($fh, $token);
-    open($fh, ">lexan.hxx") or die "chyba open()";
+    my ( $fh, $token );
+    open( $fh, ">lexan.hxx" ) or die "chyba open()";
     select $fh;
-    
+
     print "#ifndef LEXAN_HXX\n";
     print "#define LEXAN_HXX\n\n";
 
@@ -68,16 +70,16 @@ while (! $finish) {
 
     print "#define GETCHAR() lexan_input.get()\n";
     print "#define UNGETCH() lexan_input.unget()\n\n";
-    
-    print "const short POCETSTAVU = ".($state+1).";\n\n";
+
+    print "const short POCETSTAVU = " . ( $state + 1 ) . ";\n\n";
 
     print "enum Token {\n";
     print "TOK_ERROR = -1,\n";
     $token = 256;
-    for my $i (0..$state) {
-	if (defined($table[$i]{'token'})) {
-	    print $table[$i]{'token'}," = ",$token++,",\n";
-	}
+    for my $i ( 0 .. $state ) {
+        if ( defined( $table[$i]{'token'} ) ) {
+            print $table[$i]{'token'}, " = ", $token++, ",\n";
+        }
     }
     print "};\n\n";
 
@@ -92,8 +94,8 @@ while (! $finish) {
 
 # generuj lexan.cxx
 {
-    my ($fh, $token);
-    open($fh, ">lexan.cxx") or die "chyba open()";
+    my ( $fh, $token );
+    open( $fh, ">lexan.cxx" ) or die "chyba open()";
     select $fh;
 
     print "#include <lexan.hxx>\n\n";
@@ -102,28 +104,28 @@ while (! $finish) {
 
     # tabulka jmen tokenu v koncovych stavech
     print "static const char * const kstavyjmena[POCETSTAVU] =\n{\n";
-    for my $i (0..$state) {
-	print "/* $i */ ";
- 	if (defined($table[$i]{'token'})) {
- 	    print '"'.$table[$i]{'token'}.'"';
- 	}
- 	else {
- 	    print "NULL";
- 	}
- 	print ",\n";
+    for my $i ( 0 .. $state ) {
+        print "/* $i */ ";
+        if ( defined( $table[$i]{'token'} ) ) {
+            print '"' . $table[$i]{'token'} . '"';
+        }
+        else {
+            print "NULL";
+        }
+        print ",\n";
     }
     print "};\n\n";
 
     # tabulka koncovych stavu
     print "static const char kstavy[POCETSTAVU] =\n{\n";
-    for my $i (0..$state) {
-	if (defined($table[$i]{'token'})) {
-	    print "1, ";
-	}
-	else {
-	    print "0, ";
-	}
-	print "\n" if ($i+1) % 20 == 0;
+    for my $i ( 0 .. $state ) {
+        if ( defined( $table[$i]{'token'} ) ) {
+            print "1, ";
+        }
+        else {
+            print "0, ";
+        }
+        print "\n" if ( $i + 1 ) % 20 == 0;
     }
     print "\n};\n\n";
 
@@ -132,52 +134,56 @@ while (! $finish) {
     print "short poslednikstav = -1;\n";
     print "Token token = TOK_ERROR;\n";
     print "int ch;\n\n";
-    # pro kazdy stav udelej navesti
-    for my $i (0..$state) {
-	my $last = 1;
-	my $endstate;
-	my $statename = genname($table[$i]{'prefix'}, $i);
-	print "stav_".$statename.": {\n";
-	print "    /* prefix: \"".($table[$i]{'prefix'})."\" */\n";
-	# konecny stav
-	if (($endstate = defined($table[$i]{'token'}))) {
-	    print "    /* konecny stav */\n";
-	    print "    token = ".($table[$i]{'token'}).";\n";
-	    print "    poslednikstav = ".$i.";\n";
-	}
-	
-	# je to posledni stav a za nim zadny dalsi?
-	#for my $char (0..255) {
-	#    if (defined($table[$i]{'row'}[$char])) {
-	#	$last = 0;
-	#	last;
-	#    }
-	#}
 
-	# ostatni stavy
-	print "    ch = GETCHAR();\n";
-	print "    switch (ch) {\n";
-	for my $char (0..255) {
-	    if (defined($table[$i]{'row'}[$char])) {
-		print "    case '".((chr $char) eq "'" ? "\\'" : (chr $char))
-		    ."': {\n";
-		print "       goto stav_"
-		    .genname($table[$table[$i]{'row'}[$char]]{'prefix'},
-			     $table[$i]{'row'}[$char]).";\n";
-		print "    }\n";
-	    }
-	}
-	print "    default: {\n";
-	if ($endstate) {
-	    print "      UNGETCH();\n";
-	    print "      goto lexan_konec;\n";
-	}
-	else {
-	    print "      goto lexan_error;\n";
-	}
-	print "    }\n";
-	print "    }\n";
-	print "}\n\n";
+    # pro kazdy stav udelej navesti
+    for my $i ( 0 .. $state ) {
+        my $last = 1;
+        my $endstate;
+        my $statename = genname( $table[$i]{'prefix'}, $i );
+        print "stav_" . $statename . ": {\n";
+        print "    /* prefix: \"" . ( $table[$i]{'prefix'} ) . "\" */\n";
+
+        # konecny stav
+        if ( ( $endstate = defined( $table[$i]{'token'} ) ) ) {
+            print "    /* konecny stav */\n";
+            print "    token = " . ( $table[$i]{'token'} ) . ";\n";
+            print "    poslednikstav = " . $i . ";\n";
+        }
+
+        # je to posledni stav a za nim zadny dalsi?
+        #for my $char (0..255) {
+        #    if (defined($table[$i]{'row'}[$char])) {
+        #	$last = 0;
+        #	last;
+        #    }
+        #}
+
+        # ostatni stavy
+        print "    ch = GETCHAR();\n";
+        print "    switch (ch) {\n";
+        for my $char ( 0 .. 255 ) {
+            if ( defined( $table[$i]{'row'}[$char] ) ) {
+                print "    case '"
+                  . ( ( chr $char ) eq "'" ? "\\'" : ( chr $char ) )
+                  . "': {\n";
+                print "       goto stav_"
+                  . genname( $table[ $table[$i]{'row'}[$char] ]{'prefix'},
+                    $table[$i]{'row'}[$char] )
+                  . ";\n";
+                print "    }\n";
+            }
+        }
+        print "    default: {\n";
+        if ($endstate) {
+            print "      UNGETCH();\n";
+            print "      goto lexan_konec;\n";
+        }
+        else {
+            print "      goto lexan_error;\n";
+        }
+        print "    }\n";
+        print "    }\n";
+        print "}\n\n";
     }
     print "lexan_error:\n";
     print "    return TOK_ERROR;\n\n";
