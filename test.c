403c7049e6b3cc13a15fdbf7a0dd50b5a9f806b6
wilx <unknown>
2003-01-15 04:27:39 +0000

    Novy soubor.
    Novy soubor.
    Velice jednoduch test.

---
+++ test.c
@@ -0,0 +1,28 @@
+int main ()
+{
+int x, y, z;
+float f, g;
+x = 1;
+y = 2;
+z = x + y;
+
+f = 1.1;
+g = f + 2.2;
+
+while (g < 10) {
+    g += 1;
+}
+
+switch (y) {
+case 1 : {
+    z = x + y;
+}
+case 2 : {
+    z = x - y;
+}
+default: {
+}
+}
+
+}
+
